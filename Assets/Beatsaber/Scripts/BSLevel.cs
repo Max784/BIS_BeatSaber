bf377ab44b15a44c944773c506436c851574eb6b
cambryx <magnus@magnusmenger.de>
2021-06-05 13:49:21 +0200

    ez finished

---
+++ Assets/Beatsaber/Scripts/BSLevel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DualPantoFramework;
+using UnityEngine;
+
+public class BSLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Serializable]
+    public class Block
+    {
+        public float time;
+        public GameObject blockObject;
+    }
+    
+    public List<Block> block_sequence;
+    public float trace_start_time;
+    public int current_index;
+    public bool tracing;
+    public LowerHandle it_handle;
+    public BSGamemanager gamemanager;
+
+
+    public void TraceBlocks()
+    {
+        trace_start_time = Time.time;
+        tracing = true;
+        current_index = 0;
+    }
+    
+    void Start()
+    {
+        it_handle = GameObject.Find("Panto").GetComponent<LowerHandle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tracing)
+        {
+            if (Time.time > trace_start_time + block_sequence[current_index].time)
+            {
+                it_handle.SwitchTo(block_sequence[current_index].blockObject);
+                current_index++;
+                if (current_index == block_sequence.Count)
+                {
+                    tracing = false;
+                    gamemanager.StartSabering();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Beatsaber/Scripts/BSPlayer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DualPantoFramework;
+using UnityEngine;
+
+public class BSPlayer : MonoBehaviour
+{
+    public BSLevel level;
+    public float sabering_start_time;
+    public bool sabering;
+    public int current_index;
+    public float time_window = 1.0f;
+    private UpperHandle upperHandle;
+
+    void Awake()
+    {
+        upperHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+    }
+
+    public void StartSabering()
+    {
+        sabering = true;
+        sabering_start_time = Time.time;
+        current_index = 0;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = upperHandle.HandlePosition(transform.position);
+
+        if (sabering && sabering_start_time + level.block_sequence[current_index].time + time_window < Time.time)
+        {
+            Debug.Log(":( nooooo");
+            current_index++;
+            if (current_index == level.block_sequence.Count)
+            {
+                sabering = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!sabering)
+        {
+            return;
+        }
+        if (other.gameObject == level.block_sequence[current_index].blockObject)
+        {
+            if (sabering_start_time + level.block_sequence[current_index].time - time_window < Time.time)
+            {
+                Debug.Log("Yay");
+                current_index++;
+                if (current_index == level.block_sequence.Count)
+                {
+                    sabering = false;
+                }
+            }
+        }
+    }
+}
